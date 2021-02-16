223d90fc06edcc6ab40379c4e6299d4262444758
Ludovic Ledieu <lledieu@free.fr>
2021-02-16 23:53:52 +0100

    Config was missing for FS

---
+++ config/config_FS.php
@@ -0,0 +1,10 @@
+<?php
+$home = "https://familysearch.org";
+$logo = "https://edge.fscdn.org/assets/components/hf/assets/img/logos/tree-logotype-0x-5e2a1017e994b22c3c3c0b557a7e0304.png";
+$title = "Église de Jésus-Christ des saints des derniers jours";
+$ark = "61903";
+
+$step1 = "FS";
+$base1 = "$home/ark:/$ark/";
+$base2 = "https://sg30p0.familysearch.org/service/records/storage/deepzoomcloud/dz/v1/";
+?>
