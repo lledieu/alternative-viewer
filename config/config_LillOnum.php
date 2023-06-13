11095c691e03ec84294a327276e610b8e44b7c2f
Ludovic Ledieu <lledieu@free.fr>
2023-06-13 23:51:22 +0200

    Added LillOnum

---
+++ config/config_LillOnum.php
@@ -0,0 +1,16 @@
+<?php
+$home = "https://lillonum.univ-lille.fr";
+$logo = "$home/files/asset/d0121f8f58eb6bfa26b5a25231179b85fbfa35d4.png";
+$title = "LillOnum";
+$ark = "72505";
+
+// Recherche simple
+$step1 = "search";
+$s_url_prefix = "$home/s/lillonum/ark:/$ark/";
+$s_url_suffix = "";
+$s_filter = "iiif\/2\/([0-9]*)\/manifest\"";
+
+// IIIF
+$t_mode = "IIIF";
+$t_base = "$home/iiif/2/";
+?>
