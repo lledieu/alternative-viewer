4b315912cc5ccf4a135ca2fb74468e4ad59ee0e8
Ludovic Ledieu <lledieu@free.fr>
2023-06-19 11:42:05 +0200

    AM-Rennes with IIIF

--- config/config_AM-Rennes.php
+++ config/config_AM-Rennes.php
@@ -1,19 +1,17 @@
 <?php
 $home = "https://www.archives.rennes.fr";
+$logo = "$home/img/logo_accueil.png?1651762832";
+$title = "Archives de Rennes";
 $ark = "74559";
 
 // Recherche simple
 $step1 = "search";
 $s_url_prefix = "$home/archive/resultats/simple//n:19?RECH_S=%22";
 $s_url_suffix = "%22&type=simple";
-$s_filter = "href=\"\/ark:\/$ark\/([^\"]*\/dao)";
+$s_filter = "href=\"\/ark:\/$ark\/([^\"]*)\/dao";
 
-// Zomify
-$t_mode = "Zoomify";
+// IIIF
+$t_mode = "IIIF";
 $t_base = "$home/ark:/$ark/";
-$z_base = $home;
-
-$filter_logo = '"info":{"logo":"([^"]*)"';
-$filter_title = '<meta name="publisher" content="([^"]*)"';
 
 ?>
